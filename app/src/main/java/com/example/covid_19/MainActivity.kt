ad1cebe32162068dc0bcc4b382db94b3941cb940
William Wallace <wwa52@uclive.ac.nz>
2020-06-02 19:57:37 +1200

    move location getting to splash

--- app/src/main/java/com/example/covid_19/MainActivity.kt
+++ app/src/main/java/com/example/covid_19/MainActivity.kt
@@ -1,36 +1,25 @@
 package com.example.covid_19
 
-import android.annotation.SuppressLint
 import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.location.Address
-import android.location.Geocoder
-import android.location.Location
-import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Looper
-import android.provider.Settings
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.covid_19.ui.home.HomeFragment
 import com.example.covid_19.ui.news.NewsFragment
 import com.example.covid_19.ui.saved.SavedFragment
 import com.example.covid_19.ui.saved.SettingsFragment
-import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -43,7 +32,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private val fm: FragmentManager = supportFragmentManager
     var active: Fragment = homeFragment
 
-    lateinit var mFusedLocationClient: FusedLocationProviderClient
     lateinit var sharedPreference: SharedPreference
     private var CHANNEL_ID: String = "gang_channel"
     private lateinit var sensorManager: SensorManager
@@ -85,8 +73,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
 
         sharedPreference = SharedPreference(this)
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        getLastLocation()
         createNotificationChannel()
 
         // Daily notifications
@@ -132,125 +118,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
-    //Unique value
-    private val PERMISSION_ID = 42
-
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION),
-            PERMISSION_ID
-        )
-    }
-
-    private fun checkPermissions(): Boolean {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            return true
-        }
-        return false
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == PERMISSION_ID) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                // Granted. Start getting the location information
-            }
-        }
-    }
-
-    private fun isLocationEnabled(): Boolean {
-        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getLastLocation() {
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-
-                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
-                    val location: Location? = task.result
-                    if (location == null) {
-                        requestNewLocationData()
-                    } else {
-                        getCountry(location)
-                    }
-                }
-            } else {
-                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
-                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivity(intent)
-            }
-        } else {
-            requestPermissions()
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun requestNewLocationData() {
-        val mLocationRequest = LocationRequest()
-        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        mLocationRequest.interval = 0
-        mLocationRequest.fastestInterval = 0
-        mLocationRequest.numUpdates = 1
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        mFusedLocationClient.requestLocationUpdates(
-            mLocationRequest, mLocationCallback,
-            Looper.myLooper()
-        )
-    }
-
-    private val mLocationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            val mLastLocation: Location = locationResult.lastLocation
-            getCountry(mLastLocation)
-        }
-    }
-
-    private fun getCountry(location: Location) {
-        val gcd = Geocoder(this, Locale.getDefault())
-        val addresses: List<Address> =
-            gcd.getFromLocation(location.latitude, location.longitude, 1)
-
-        if (addresses.isNotEmpty()) {
-            val countryFlag: String = addresses[0].countryCode.toFlagEmoji()
-            val country: String = addresses[0].getCountryName()
-            val countryISO: String = addresses[0].countryCode // Country ISO code for newsapi.org
-            sharedPreference.save("COUNTRY", country)
-            sharedPreference.save("COUNTRY_ISO", countryISO)
-            sharedPreference.save("COUNTRY_KEBAB_CASE", country.toKebabCase()) // Kebab case version of country name needed for Covid API
-            sharedPreference.save("COUNTRY_FLAG", countryFlag)
-        }
-
-    }
-
-    //Function to get flag emoji from country code
-    private fun String.toFlagEmoji(): String {
-        // 1. It first checks if the string consists of only 2 characters: ISO 3166-1 alpha-2 two-letter country codes (https://en.wikipedia.org/wiki/Regional_Indicator_Symbol).
-        if (this.length != 2) {
-            return this
-        }
-
-        val countryCodeCaps = this.toUpperCase() // upper case is important because we are calculating offset
-        val firstLetter = Character.codePointAt(countryCodeCaps, 0) - 0x41 + 0x1F1E6
-        val secondLetter = Character.codePointAt(countryCodeCaps, 1) - 0x41 + 0x1F1E6
-
-        // 2. It then checks if both characters are alphabet
-        if (!countryCodeCaps[0].isLetter() || !countryCodeCaps[1].isLetter()) {
-            return this
-        }
-
-        return String(Character.toChars(firstLetter)) + String(Character.toChars(secondLetter))
-    }
-
-    private fun String.toKebabCase(): String {
-        return this.toLowerCase().replace(" ", "-")
-    }
-
     override fun onResume() {
         super.onResume()
         sensorManager.registerListener(this, temperature, SensorManager.SENSOR_DELAY_NORMAL)
@@ -267,7 +134,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?) {
         val ambient_temp = event?.values?.get(0);
         val tempText: TextView = findViewById(R.id.tempText)
-        tempText.text = "${ambient_temp.toString()} °C"
+        tempText.text = ambient_temp.toString() + " °C"
     }
 
 
--- app/src/main/java/com/example/covid_19/SplashActivity.kt
+++ app/src/main/java/com/example/covid_19/SplashActivity.kt
@@ -1,21 +1,186 @@
 package com.example.covid_19
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import java.util.*
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_TIME_OUT: Long = 3000
+    //Unique value
+    private val PERMISSION_ID = 42
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    lateinit var sharedPreference: SharedPreference
+    private val SPLASH_TIME_OUT: Long = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, SPLASH_TIME_OUT)
+        sharedPreference = SharedPreference(this)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocation()
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            PERMISSION_ID
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Granted. Start getting the location information
+                startActivity(Intent(this, MainActivity::class.java))
+
+            }
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        getCountry(location)
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            getCountry(mLastLocation)
+
+        }
+    }
+
+    private fun getCountry(location: Location) {
+        val gcd = Geocoder(this, Locale.getDefault())
+        val addresses: List<Address> =
+            gcd.getFromLocation(location.latitude, location.longitude, 1)
+
+        if (addresses.isNotEmpty()) {
+            val countryFlag: String = addresses[0].countryCode.toFlagEmoji()
+            val country: String = addresses[0].getCountryName()
+            val countryISO: String = addresses[0].countryCode // Country ISO code for newsapi.org
+            sharedPreference.save("COUNTRY", country)
+            sharedPreference.save("COUNTRY_ISO", countryISO)
+            sharedPreference.save(
+                "COUNTRY_KEBAB_CASE",
+                country.toKebabCase()
+            ) // Kebab case version of country name needed for Covid API
+            sharedPreference.save("COUNTRY_FLAG", countryFlag)
+
+            Handler().postDelayed({
+                // This method will be executed once the timer is over
+                // Start your app main activity
+
+                startActivity(Intent(this, MainActivity::class.java))
+
+                // close this activity
+                finish()
+            }, SPLASH_TIME_OUT)
+
+        }
+
+    }
+
+    //Function to get flag emoji from country code
+    private fun String.toFlagEmoji(): String {
+        // 1. It first checks if the string consists of only 2 characters: ISO 3166-1 alpha-2 two-letter country codes (https://en.wikipedia.org/wiki/Regional_Indicator_Symbol).
+        if (this.length != 2) {
+            return this
+        }
+
+        val countryCodeCaps =
+            this.toUpperCase() // upper case is important because we are calculating offset
+        val firstLetter = Character.codePointAt(countryCodeCaps, 0) - 0x41 + 0x1F1E6
+        val secondLetter = Character.codePointAt(countryCodeCaps, 1) - 0x41 + 0x1F1E6
+
+        // 2. It then checks if both characters are alphabet
+        if (!countryCodeCaps[0].isLetter() || !countryCodeCaps[1].isLetter()) {
+            return this
+        }
+
+        return String(Character.toChars(firstLetter)) + String(Character.toChars(secondLetter))
+    }
+
+    private fun String.toKebabCase(): String {
+        return this.toLowerCase().replace(" ", "-")
     }
 }
